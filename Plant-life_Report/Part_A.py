00aed3d10291ed4b4331d2f816baef8d15b5cdda
AyTanase <ay.tanase@gmail.com>
2017-12-13 18:06:32 +0900

    end A.1

---
+++ Plant-life_Report/Part_A.py
@@ -0,0 +1,14 @@
+﻿from traceback import print_exc
+
+# 1.
+from pandas import read_csv
+from IPython.display import display
+
+try:
+    plants2017 = read_csv(r"environmental_survey\plants2017.csv")
+    plants2017 = plants2017.ix[plants2017["Plant"] == "shrub"].reset_index(drop=True)
+    display(plants2017)
+except:
+    print_exc()
+finally:
+    input("Press any key to continue...")
