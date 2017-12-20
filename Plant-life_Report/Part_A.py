19cae86e2076599e7ffe116d918fac1b29bc55b6
AyTanase <ay.tanase@gmail.com>
2017-12-20 17:23:41 +0900

    Change Plant-life_Report/Part_A.py
    change_gps_data:
        change to radian
        add unit to name
    
    add remove_below

--- Plant-life_Report/Part_A.py
+++ Plant-life_Report/Part_A.py
@@ -1,15 +1,22 @@
 ﻿from numpy import cos
+from numpy import pi
 
 # (1)
 def remove_tree(df):
-    df = df.ix[df["Plant"] == "shrub"].reset_index(drop=True)
+    return df.ix[df["Plant"] == "shrub"]\
+        .reset_index(drop=True)
 
 # (2)
 def change_gps_data(df):
     if "GPS_lon" in df.keys():
-        df["GPS_lon"] = 40075160 * df["GPS_lon"] * cos(df["GPS_lat"]) / 360
-        df["GPS_lat"] = 40008000 * df["GPS_lat"] / 360
-        df = df.rename(index=str, columns={"GPS_lat": "lat", "GPS_lon": "lon"})
+        df["GPS_lon"] *= 40075160 * cos(df["GPS_lat"] * pi / 180) / 360
+        df["GPS_lat"] *= 40008000 / 360
+        return df.rename(index=str, columns={"GPS_lat": "lat_m", "GPS_lon": "lon_m"})
+
+# (3)
+def remove_below(df, height=0.5):
+    return df.ix[df["height_m"] >= height]\
+        .reset_index(drop=True)
 
 if __name__ == "__main__":
     from pandas import read_csv
@@ -17,9 +24,13 @@ if __name__ == "__main__":
     from traceback import print_exc
 
     try:
-        plants2017 = read_csv(r"environmental_survey\plants2017")
-        remove_tree(plants2017)
-        change_gps_data(plants2017)
+        plants2017 = read_csv(r"environmental_survey\plants2017.csv")
+        display(plants2017)
+        plants2017 = remove_tree(plants2017)
+        display(plants2017)
+        plants2017 = change_gps_data(plants2017)
+        display(plants2017)
+        plants2017 = remove_below(plants2017)
         display(plants2017)
     except:
         print_exc()
