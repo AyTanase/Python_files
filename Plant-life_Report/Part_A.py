437f2756ea61ec2045df882bcbb62560d2393516
AyTanase <ay.tanase@gmail.com>
2017-12-20 17:40:42 +0900

    Change Plant-life_Report/Part_A.py
    change change_gps_data
        change to km
    
    finish Part A
    
    start Part B

--- Plant-life_Report/Part_A.py
+++ Plant-life_Report/Part_A.py
@@ -1,6 +1,7 @@
 ﻿from numpy import cos
 from numpy import pi
 
+# Part A
 # (1)
 def remove_tree(df):
     return df.ix[df["Plant"] == "shrub"]\
@@ -9,9 +10,14 @@ def remove_tree(df):
 # (2)
 def change_gps_data(df):
     if "GPS_lon" in df.keys():
-        df["GPS_lon"] *= 40075160 * cos(df["GPS_lat"] * pi / 180) / 360
-        df["GPS_lat"] *= 40008000 / 360
-        return df.rename(index=str, columns={"GPS_lat": "lat_m", "GPS_lon": "lon_m"})
+        df["GPS_lon"] *= 40075.160 * cos(df["GPS_lat"] * pi / 180) / 360
+        df["GPS_lat"] *= 40008.000 / 360
+
+        # You can change df's members, but you cannot change df itself.
+        # df may be the copy of the pointer to the DataFrame.
+        return df.rename(index=str, columns={"GPS_lat": "lat_km", "GPS_lon": "lon_km"})
+    else:
+        return df
 
 # (3)
 def remove_below(df, height=0.5):
@@ -24,15 +30,27 @@ if __name__ == "__main__":
     from traceback import print_exc
 
     try:
-        plants2017 = read_csv(r"environmental_survey\plants2017.csv")
-        display(plants2017)
+        path = "environmental_survey\\"
+        plants2017 = read_csv(path + "plants2017.csv")
+        #print("Original Data:")
+        #display(plants2017)
+
         plants2017 = remove_tree(plants2017)
-        display(plants2017)
+        #print("\nRemove Tree")
+        #display(plants2017)
+
         plants2017 = change_gps_data(plants2017)
-        display(plants2017)
+        #print("\nChange GPS Data to meter")
+        #display(plants2017)
+
         plants2017 = remove_below(plants2017)
-        display(plants2017)
+        #print("\nRemove below 0.5m height")
+        #display(plants2017)
+
+        pH = read_csv(path + "pH_2017.csv")
+        pH = change_gps_data(pH)
+        display(pH)
     except:
         print_exc()
     finally:
-        input("Press any key to continue...")
+        input("\nPress any key to continue...")
