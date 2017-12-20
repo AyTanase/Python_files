0409aa9234229b11d461eaf46dd57e78d8c74b52
AyTanase <ay.tanase@gmail.com>
2017-12-20 17:01:06 +0900

    make functions Plant-life_Report/Part_A.py

--- Plant-life_Report/Part_A.py
+++ Plant-life_Report/Part_A.py
@@ -1,14 +1,27 @@
-﻿from traceback import print_exc
+﻿from numpy import cos
 
-# 1.
-from pandas import read_csv
-from IPython.display import display
+# (1)
+def remove_tree(df):
+    df = df.ix[df["Plant"] == "shrub"].reset_index(drop=True)
 
-try:
-    plants2017 = read_csv(r"environmental_survey\plants2017.csv")
-    plants2017 = plants2017.ix[plants2017["Plant"] == "shrub"].reset_index(drop=True)
-    display(plants2017)
-except:
-    print_exc()
-finally:
-    input("Press any key to continue...")
+# (2)
+def change_gps_data(df):
+    if "GPS_lon" in df.keys():
+        df["GPS_lon"] = 40075160 * df["GPS_lon"] * cos(df["GPS_lat"]) / 360
+        df["GPS_lat"] = 40008000 * df["GPS_lat"] / 360
+        df = df.rename(index=str, columns={"GPS_lat": "lat", "GPS_lon": "lon"})
+
+if __name__ == "__main__":
+    from pandas import read_csv
+    from IPython.display import display
+    from traceback import print_exc
+
+    try:
+        plants2017 = read_csv(r"environmental_survey\plants2017")
+        remove_tree(plants2017)
+        change_gps_data(plants2017)
+        display(plants2017)
+    except:
+        print_exc()
+    finally:
+        input("Press any key to continue...")
