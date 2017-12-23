0189252b8b9bfab9a473e4fc1ac36510126f568d
AyTanase <ay.tanase@gmail.com>
2017-12-23 14:23:27 +0900

    show plot from console Input_and_Output_Exercises/manipulating_dataframe.py

--- Input_and_Output_Exercises/manipulating_dataframe.py
+++ Input_and_Output_Exercises/manipulating_dataframe.py
@@ -1,6 +1,13 @@
 ﻿from pandas import read_csv
-headers = ["X", "Y"]
-data = read_csv(r"..\..\ILAS_python\sample_data\noHeader_noIndex_vert.csv", names=headers)
-data.insert(loc=2, column="Z", value=data["X"]*(data["Y"]+50))
-data.plot("X", "Z", kind="scatter")
-input("Press Any Key to Continue...")
+import matplotlib.pyplot as plt
+from traceback import print_exc
+try:
+    headers = ["X", "Y"]
+    data = read_csv(r"..\..\ILAS_python\sample_data\noHeader_noIndex_vert.csv", names=headers)
+    data.insert(loc=2, column="Z", value=data["X"]*(data["Y"]+50))
+    data.plot("X", "Z", kind="scatter")
+    plt.show()
+except:
+    print_exc()
+finally:
+    input("Press Any Key to Continue...")
